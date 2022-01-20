ebe18a6de0108acd2623060c2f0bc57bc7757ceb
shinnthantminn <shinthantmin32@gmail.com>
2022-01-21 00:50:19 +0630

    main v2

--- dist/Script/Script.js
+++ dist/Script/Script.js
@@ -16,3 +16,24 @@ function showHide(x) {
     menu.classList.toggle("top-170");
     menu.classList.toggle("top-0");
 }
+
+/*for hover svg changes fill other color*/
+
+const forFill = document.querySelector("#forFill");
+const motherFill = document.querySelector("#motherFill");
+
+motherFill.addEventListener("mouseenter",_=>{
+    forFill.classList.toggle("fill-darkGray-100");
+});
+
+motherFill.addEventListener("mouseleave",_=>{
+    forFill.classList.toggle("fill-darkGray-100");
+});
+
+/*slider*/
+new Glide('.glide',{
+    autoplay:5000,
+    gap:0
+}).mount()
+
+
